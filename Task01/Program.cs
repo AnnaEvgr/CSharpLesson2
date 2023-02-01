82db7ea47802cb09fd9e2630d12831aec622c6c3
AnnaEvgrafova <eaa79evgrafova@yandex.ru>
2023-02-01 21:46:51 +0500

    Урок 2

---
+++ Task01/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Задача 1: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+int InputInt(string message)
+{
+    System.Console.WriteLine(message);
+    int a = Convert.ToInt32(Console.ReadLine());
+    return a;
+}
+int a = InputInt("Введите трехзначное число");
+
+if (a > 99 && a < 1000)
+{
+    int result = ((a/10) % 10);
+    System.Console.WriteLine("Вторая цифра равна " + result);
+}
+else
+{
+    System.Console.WriteLine("Число не трехзначное");
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+
+78 -> третьей цифры нет
+
+32679 -> 6
+*/
+
+System.Console.WriteLine("Введите любое число ");
+string valuestring = Console.ReadLine();
+char[] ch = valuestring.ToCharArray();
+if (ch.Length >= 3)
+{
+    System.Console.WriteLine($"Третья цифра {ch[2]}");
+}
+else
+{
+    System.Console.WriteLine($"Третьей цифры нет");
+}
